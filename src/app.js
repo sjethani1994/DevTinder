ef5a71efd5e15377500524c956d762da4d4d683c
Sumit Jethani <sjethani651@gmail.com>
2026-09-06 19:46:51 +0530

    session on Sunday 05-08-2026

--- src/app.js
+++ src/app.js
@@ -2,11 +2,40 @@ const http = require("http");
 const Express = require("express");
 
 const app = Express();
+
 const server = http.createServer(app);
 
-app.use("/get", (req, res) => {
-  res.send("Hello, World.......................!");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "John", lastName: "Doe" });
 });
+
+app.post("/user", (req, res) => {
+  res.send({ message: "Data is saved successfully" });
+});
+
+app.delete("/delete", (req, res) => {
+  res.send({ message: "User deleted successfully" });
+});
+
+app.get("/user/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send({ message: `User with ID ${userId} is fetched successfully` });
+});
+
+app.get("/userById", (req, res) => {
+  const userId = req.query.userId;
+  res.send({ message: `User with ID ${userId} is fetched successfully` });
+});
+
+// app.get(["/ac", "/abc", "/ab"], (req, res) => {
+//   res.send({ message: "This route matches /ac and /abc and /ab" });
+// });
+
+// app.get(/^\/ab+c$/, (req, res) => {
+//   res.send({
+//     message: "Matched the route"
+//   });
+// });
 
 app.use("/", (req, res) => {
   res.send("Hello, World!");
