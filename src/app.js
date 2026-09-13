4dfd1718ddd97bdab6f03549eeed0615e6119ebb
Sumit Jethani <sjethani651@gmail.com>
2026-09-13 10:48:47 +0530

    multiple routes

--- src/app.js
+++ src/app.js
@@ -5,41 +5,18 @@ const app = Express();
 
 const server = http.createServer(app);
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "John", lastName: "Doe" });
-});
+// app.use(Express.json());
 
-app.post("/user", (req, res) => {
-  res.send({ message: "Data is saved successfully" });
-});
-
-app.delete("/delete", (req, res) => {
-  res.send({ message: "User deleted successfully" });
-});
-
-app.get("/user/:id", (req, res) => {
-  const userId = req.params.id;
-  res.send({ message: `User with ID ${userId} is fetched successfully` });
-});
-
-app.get("/userById", (req, res) => {
-  const userId = req.query.userId;
-  res.send({ message: `User with ID ${userId} is fetched successfully` });
-});
-
-// app.get(["/ac", "/abc", "/ab"], (req, res) => {
-//   res.send({ message: "This route matches /ac and /abc and /ab" });
-// });
-
-// app.get(/^\/ab+c$/, (req, res) => {
-//   res.send({
-//     message: "Matched the route"
-//   });
-// });
-
-app.use("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    res.send("User1 route");
+    next();
+  },
+  (req, res) => {
+    res.send("User2 route");
+  },
+);
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
