8368a9ef0f95dc68dab281a98db3087231ed5508
Sumit Jethani <sjethani651@gmail.com>
2026-09-14 13:52:02 +0530

    Created API's

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ app.use(Express.json());
 
 app.post("/api/signup", async (req, res) => {
   try {
-    const { firstName, lastName, email, password, role, gender, age } = req.body;
+    const { firstName, lastName, email, password, role, gender, age } =
+      req.body;
     // create a new user instance of User Schema / User Model and save it to the database
     if (!firstName || !lastName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
@@ -33,6 +34,64 @@ app.post("/api/signup", async (req, res) => {
   }
 });
 
+app.get("/api/getUserByEmailId", async (req, res) => {
+  try {
+    const users = await User.find({ email: req.body.emailId });
+
+    if (!users || users.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({
+      message: "Users fetched successfully",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      message: "Users fetched successfully",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.delete("/api/deleteUser/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.patch("/api/updateUser", async (req, res) => {
+  try {
+    const updateData= req.body;
+    console.log("updateData", req.body);
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: updateData.id },
+      updateData,
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 app.use("/", (err, req, res, next) => {
   if (err) {
